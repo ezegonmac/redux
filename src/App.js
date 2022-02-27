71970609d70ac5e4e0ffae186c9d3ff526f8dede
EGM <ezegonmac01@gmail.com>
2022-02-27 14:49:15 +0100

    First steps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+export const reducer = (state = 0, action) => {
+  console.log({ action, state })
+  switch (action.type) {
+    case 'incrementar':
+      return state + 1
+    case 'decrementar':
+      return state - 1
+    case 'set':
+      return action.payload
+    default:
+      return state
+  }
+} 
 
 function App() {
+  const [valor, setValor] = useState('')
+  const dispatch = useDispatch()
+  const state = useSelector(state => state)
+  const set = () => {
+    dispatch({ type: 'set', payload: valor})
+    setValor('')
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Contador: {state}</p>
+      <button onClick={() => dispatch({ type: 'incrementar'})}>Incrementar</button>
+      <button onClick={() => dispatch({ type: 'decrementar'})}>Decrementar</button>
+      <button onClick={set}>Set</button>
+      <input value={valor} onChange={e => setValor(Number(e.target.value))}/>
     </div>
   );
 }
